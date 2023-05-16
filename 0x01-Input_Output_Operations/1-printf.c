f40bf319362aa3d7c74e161d65f72610a4690f7b
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-05-16 22:18:25 +0300

    puts

--- 0x01-Input_Output_Operations/1-printf.c
+++ 0x01-Input_Output_Operations/1-printf.c
@@ -4,25 +4,29 @@
 int main(int argc, char *argv[])
 {
 
-	//unsigned values
+	// unsigned values
 	unsigned int a = 77;
 	printf("This is an unsigned integer: %u\n", a);
-	//Floats
+
+	// Floats
 	float f = 45.7;
 	printf("This is a float: %.2f\n", f);
-	//64 bit characters
+
+	// 64 bit characters
 	long long int b = 77584749373;
-	printf("This is a 64 bit integer: %lld\n",b);
-	//Align things
+	printf("This is a 64 bit integer: %lld\n", b);
+
+	// Align things
 	int g = 7800;
 	int h = 8475948;
-	printf("%10d\n%10d\n",g, h);
-	//printing in hexadecimals
+	printf("%10d\n%10d\n", g, h);
+	
+	// printing in hexadecimals
 	int v = 12;
 	printf("The number 12 in hexadecimals: %x\n", v);
 	printf("The number 12 in hexadecimal capital: %X\n", v);
 	printf("The number 12 in hex with the ox prefix: %#x\n", v);
-	printf("Actual hex values: %08x\n",v);
+	printf("Actual hex values: %08x\n", v);
 	printf("Print an address of v : %p\n", &v);
 
 	return 0;
---
+++ 0x01-Input_Output_Operations/7-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(int argc, char *argv[])
+{
+    char str[] = "Hello World!";
+
+    puts(str);
+
+    return 0;
+}
